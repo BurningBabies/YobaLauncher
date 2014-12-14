1056a3d48ec5745a8a3fe0963ab32a49531ce234
TideSofDarK <tidesmain@gmail.com>
2014-12-14 11:02:39 +0500

    Bar and basic animations

---
+++ YobaLauncher/Assets/Scripts/Bar.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using DG.Tweening;
+
+public class Bar : MonoBehaviour {
+
+    public RectTransform mask;
+
+    float startWidth;
+
+    public float speed = 1100f;
+    void Start()
+    {
+        //DOTween.Init(false, false, LogBehaviour.ErrorsOnly);
+
+        startWidth = mask.sizeDelta.x;
+
+        StartCoroutine(PingPong());
+    }
+
+    IEnumerator PingPong()
+    {
+        bool left = true;
+        while (true)
+        {
+            if (left)
+            {
+                Vector2 sizeDelta = mask.sizeDelta;
+                sizeDelta.x = Mathf.MoveTowards(sizeDelta.x, 0f, Time.deltaTime * speed);
+                mask.sizeDelta = sizeDelta;
+                if (sizeDelta.x <= 0f)
+                {
+                    left = false;
+                }
+            }
+            if (!left)
+            {
+                Vector2 sizeDelta = mask.sizeDelta;
+                sizeDelta.x = Mathf.MoveTowards(sizeDelta.x, startWidth, Time.deltaTime * speed);
+                mask.sizeDelta = sizeDelta;
+                if (sizeDelta.x >= startWidth)
+                {
+                    left = true;
+                }
+            }
+            yield return null;
+        }
+    }
+	
+	void Update () {
+	}
+}
---
+++ YobaLauncher/Assets/Scripts/FixPosition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixPosition : MonoBehaviour {
+
+    private Vector3 pos;
+
+    void LateUpdate()
+    {
+        if (pos == Vector3.zero) pos = GetComponent<RectTransform>().position;
+        GetComponent<RectTransform>().position = pos;
+	}
+}
